543472a601316b31e950554cdddd05c7e1850a7d
effective-range <info@effective-range.com>
2025-03-18 00:36:02 +0100

    fix: add multi-distro support to CI workflow and update test script usage instructions

--- test.sh
+++ test.sh
@@ -3,26 +3,37 @@
 # SPDX-License-Identifier: MIT
 
 set -e
+ROOT_DIR="$(dirname $0)"
 BUILD_ARCH="$1"
-TARGET="$2"
+DISTRO="$2"
 
 if [ -z "$BUILD_ARCH" ]
 then
-echo "Usage: $0  <BUILD_ARCH> [<TARGET>]"
+echo "Usage: $0  <BUILD_ARCH> [<DISTRO>]"
 exit 1
 fi
-
-if [ -z "$TARGET" ]
-then
-echo "Usage: $0  [<TARGET>]"
-exit 1
-fi
-
-DISTRO=$(grep "VERSION_CODENAME=" $(dirname $0)/TARGET/$TARGET/target | cut -d "=" -f 2)
 
 if [ -z "$DISTRO" ]
 then
-echo "Not able to determine the distro from file $(dirname $0)/TARGET/$TARGET/target"
+echo "Usage: $0  [<DISTRO>]"
+exit 1
+fi
+
+if [ $BUILD_ARCH == "amd64" ]
+then
+TARGET=ARMHF-${DISTRO^^}
+elif [ $BUILD_ARCH == "arm64" ]
+then
+TARGET=AARCH64-${DISTRO^^}
+else
+TARGET=$BUILD_ARCH-${DISTRO^^}
+fi
+
+
+
+if [ ! -d "$ROOT_DIR/TARGET/$TARGET" ]
+then
+echo "Target directory $TARGET does not exist!"
 exit 1
 fi
 
@@ -30,16 +41,16 @@ fi
 
 python3 -m venv /tmp/testvenv
 
-/tmp/testvenv/bin/python3 -m pip install $(dirname $0)/build_tools/dpkgdeps_src
-/tmp/testvenv/bin/python3 -m unittest discover -s $(dirname $0)/build_tools/dpkgdeps_src/test/  -p *test.py
+/tmp/testvenv/bin/python3 -m pip install $ROOT_DIR/build_tools/dpkgdeps_src
+/tmp/testvenv/bin/python3 -m unittest discover -s $ROOT_DIR/build_tools/dpkgdeps_src/test/  -p *test.py
 
 IMG_ID=$(uuidgen)
 
-make -C "$(dirname $0)" base-$BUILD_ARCH TARGET_NAME=$TARGET CROSS_BASE_IMAGE_VER=$DISTRO-slim IMG_TAG=$IMG_ID
+make -C "$ROOT_DIR" base-$BUILD_ARCH TARGET_NAME=$TARGET CROSS_BASE_IMAGE_VER=$DISTRO-slim IMG_TAG=$IMG_ID
 
-make -C "$(dirname $0)" devc-$BUILD_ARCH  IMG_TAG=$IMG_ID BASE_IMAGE_VER=$IMG_ID CROSS_BASE_IMAGE_VER=$DISTRO-slim
+make -C "$ROOT_DIR" devc-$BUILD_ARCH  IMG_TAG=$IMG_ID BASE_IMAGE_VER=$IMG_ID CROSS_BASE_IMAGE_VER=$DISTRO-slim
 
-make -C "$(dirname $0)/test" clean test BASE_IMAGE_REPO=effectiverange/er-devc-$BUILD_ARCH-$DISTRO   BASE_IMAGE_VER=$IMG_ID 
+make -C "$ROOT_DIR/test" clean test BASE_IMAGE_REPO=effectiverange/er-devc-$BUILD_ARCH-$DISTRO   BASE_IMAGE_VER=$IMG_ID 
 
 
 
